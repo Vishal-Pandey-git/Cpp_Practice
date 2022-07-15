f17c90704ee2b8254ad361ee81869139fd8bfea3
Vishal Pandey <59437588+Vishal-Pandey-git@users.noreply.github.com>
2022-07-16 00:42:59 +0530

    gg

--- 05 Use of sizeof().cpp
+++ 05 Use of sizeof().cpp
@@ -10,6 +10,6 @@ bool x=true;
 string f = "HJUGBUVCdfgehg287523 ey wyr87qyr8yr8qy9(^*^&%$^&%$%^$&^YCFGTfgtctr";
 cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl<<e<<endl<<f<<endl<<x<<endl;
 cout<<sizeof(a)<<endl<<sizeof(b)<<endl<<sizeof(c)<<endl<<sizeof(d)<<endl<<sizeof(e)<<endl<<sizeof(f)<<endl<<sizeof(x)<<endl;
-//sizeof(int) or sizeof(char) or etc... also works
+//sizeof(int) or sizeof(char) or etc... also works to know size of the datatype keyword.
 return 0;
 }
