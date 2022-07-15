9f30e65d7027f9774357e12fa7cccdcf408ca0db
Vishal Pandey <59437588+Vishal-Pandey-git@users.noreply.github.com>
2022-07-16 00:38:41 +0530

    typecastin 1

--- 06 Typecasting.cpp
+++ 06 Typecasting.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-
+int a=97;
+char c='f';
+float d=30.332;
+cout<<a<<"\t"<<c<<"\t"<<d<<endl;
+cout<<"typecasting"<<endl;
+char x=a;
+int y=c;
+cout<<x<<"\t"<<y<<endl;
+cout<<"Inplace typecasting"<<endl;
+cout<<(char)a<<"\t"<<(double)c<<"\t"<<(int)d<<endl;
+return 0;
 }
